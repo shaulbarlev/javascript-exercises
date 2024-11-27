4f33d878919b145e10c4b55fbcfecfba8301f181
Shaul Bar-Lev <shaulbarlev1@gmail.com>
2024-11-27 14:52:51 +0200

    calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a,b) {
+  //adds a and b
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	//subtracts b from a
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	// sums an array of numbers
+  return array.reduce((sum, value) => {
+    return sum += value;
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  // multiplies an array of numbers in each other
+  return array.reduce((result, value) => result * value, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let result = 1;
+  for (let i = n; i > 0; i--) {
+    result = result * i;
+  }
 };
 
 // Do not edit below this line
